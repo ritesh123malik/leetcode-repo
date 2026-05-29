164b07dbc989cb32027646a4aa461933bbdb3118
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-05-29 12:02:48 +0530

    LeetCode: Sync solution for [0145] Binary Tree Postorder Traversal (C++)

---
+++ 0145-binary-tree-postorder-traversal/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void postorder(TreeNode* node) {
+        if (node == NULL) return;
+
+        postorder(node->left);
+        postorder(node->right);
+        ans.push_back(node->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        postorder(root);
+        return ans;
+    }
+};
