55f558a504adb990bfc0ae4e8681fb92cbdfd18d
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-05-29 12:01:26 +0530

    LeetCode: Sync solution for [0167] Two Sum II - Input Array Is Sorted (C++)

---
+++ 0167-two-sum-ii-input-array-is-sorted/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0;
+        int n=numbers.size();
+        int r=n-1;
+        int sum=0;
+        while(l<=r){
+            sum=numbers[l]+numbers[r];
+
+            if(sum==target){
+                return {l+1,r+1};
+            }else if(sum<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return {};
+    }
+};
