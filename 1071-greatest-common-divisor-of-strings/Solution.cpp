242884770d9261a97de2d23f35b120b031f30b44
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-06-13 00:49:07 +0530

    LeetCode: Sync solution for [1071] Greatest Common Divisor of Strings (C++)

---
+++ 1071-greatest-common-divisor-of-strings/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1){
+            return "";
+        }
+        string str3=str1+str2;
+        int n1=str1.size();
+        int n2=str2.size();
+        int n=gcd(n1,n2);
+        return str3.substr(0,n);
+    }
+};
