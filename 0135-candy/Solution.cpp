74d229dc8ed1fd5e189120e2af88aa5db52bfd68
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-06-13 00:47:59 +0530

    LeetCode: Sync solution for [0135] Candy (C++)

---
+++ 0135-candy/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> candies(n,1);
+        int i;
+        for(i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        for(i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i]=max(candies[i],candies[i+1]+1);
+            }
+        }
+        int ans = 0;
+
+        for(int x : candies){
+
+            ans += x;
+        }
+
+        return ans;
+    }
+};
